ad891b637cc927314cae976696f48b8f8026c5cd
Glitch (hello-express) <none>
2020-11-27 10:05:45 +0000

    🏨❄️ Checkpoint ./db.json:25419550/96 ./views/index.pug:25419550/3 ./server.js:25419550/668

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 // database
-db.defaults({ books: [] })
-  .write()
+db.defaults({ books: [] }).write();
 //
 app.get('/', (req, res) => {
   res.render('home');
@@ -39,17 +38,17 @@ app.get("/books/create",(req, res) => {
 app.post("/books/create", (req, res) => {
   req.body.id = shortid.generate();
     db.get('books').push(req.body).write();
+  
     res.redirect("/books");
   
 });
+// xóa todo list
 app.get("/books/:id", function(req, res) {
-  var id = req.params.id;
-  // var user1 = db
-  //   .get("user")
-  //   .filter({ id: id })
-  //   .value();
-
-  db.get("books").remove({ id: id }).write();
-  res.redirect("/books");
+ var id = req.params.id;
+  db.get("user")
+    .remove({ id: id })
+    .write();
+  
+  res.redirect("back");
 });
 
